c1f6b5db830b4b5479b1645871cb0a3b2c821e13
Chris Grocott <chris.grocott@gmail.com>
2016-01-12 14:28:22 +0000

    Test commit with IntelliJ

---
+++ sandbox/src/Client.java
@@ -0,0 +1,8 @@
+/**
+ * Created by chris on 12/01/2016.
+ */
+public class Client {
+    public static void main(String[] args) {
+        SomeInterface a = new SomeInterfaceImpl();
+    }
+}
---
+++ sandbox/src/SomeInterface.java
@@ -0,0 +1,5 @@
+/**
+ * Created by chris on 12/01/2016.
+ */
+public interface SomeInterface {
+}
