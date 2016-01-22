4abeb06220b0b37564722f90443a7a1f10da5d72
Chris Grocott <chris.grocott@gmail.com>
2016-01-22 13:15:40 +0000

    Added adapter classes

---
+++ week3/src/adapter/PayDImpl.java
@@ -0,0 +1,62 @@
+package adapter;
+
+/**
+ * Created by chris on 22/01/2016.
+ */
+public class PayDImpl implements PayD {
+    private String custCardNo;
+    private String cardOwnerName;
+    private String cardExpMonthDate;
+    private Integer cVVNo;
+    private Double totalAmount;
+
+    @Override
+    public String getCustCardNo() {
+        return custCardNo;
+    }
+
+    @Override
+    public void setCustCardNo(String custCardNo) {
+        this.custCardNo = custCardNo;
+    }
+
+    @Override
+    public String getCardOwnerName() {
+        return cardOwnerName;
+    }
+
+    @Override
+    public void setCardOwnerName(String cardOwnerName) {
+        this.cardOwnerName = cardOwnerName;
+    }
+
+    @Override
+    public String getCardExpMonthDate() {
+        return cardExpMonthDate;
+    }
+
+    @Override
+    public void setCardExpMonthDate(String cardExpMonthDate) {
+        this.cardExpMonthDate = cardExpMonthDate;
+    }
+
+    @Override
+    public Integer getCVVNo() {
+        return cVVNo;
+    }
+
+    @Override
+    public void setCVVNo(Integer cVVNo) {
+        this.cVVNo = cVVNo;
+    }
+
+    @Override
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    @Override
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+}
