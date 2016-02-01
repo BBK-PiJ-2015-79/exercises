d4ed344207393a9f46d25d92632f120a983c27db
Chris Grocott <chris.grocott@gmail.com>
2016-02-01 15:26:02 +0000

    Added implementation of execute method in DivideInstruction

--- cw-one/smltest/sml/DivideInstructionTest.java
+++ cw-one/smltest/sml/DivideInstructionTest.java
@@ -5,9 +5,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by chris on 01/02/2016.
- */
 public class DivideInstructionTest {
     private Machine m;
 
@@ -21,7 +18,7 @@ public class DivideInstructionTest {
     public void TestDivideInstruction() {
         m.getRegisters().setRegister(0, 10);
         m.getRegisters().setRegister(1, 5);
-        Instruction subtr = new MultiplyInstruction("sub", 2, 0, 1);
+        Instruction subtr = new DivideInstruction("sub", 2, 0, 1);
         subtr.execute(m);
         assertEquals(2, m.getRegisters().getRegister(2));
     }
